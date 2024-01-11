ae6e17a4c23a982ee22a31309735023e1c9077ca
nolan <nolanvan2004@gmail.com>
2024-01-10 22:25:24 -0500

    Initial Start

---
+++ vanNolanSE1.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+struct message{
+
+
+
+
+};
+
+char input[1000];
---
+++ vanNolanSE1main.c
@@ -0,0 +1,18 @@
+
+#include "vanNolanSE1.h"
+
+int main(int argc, char * argv[]){
+
+
+
+printf("\nSimple Encyption v.1\n");
+printf("\nInput Message: ");
+
+fgets(input, sizeof(input), stdin);
+
+printf("%s", input);
+
+
+return 0;
+
+}
