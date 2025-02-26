6d47c93f8c39ecb2f4e4c96a919f67db058ba11b
Nolan <96299649+VNolanV@users.noreply.github.com>
2025-02-26 02:54:50 -0500

    Beginning development from scratch
    After deciding to resume development of this personal project in my free time, I realised that I had poorly planned the development timeline of the project. Thus, the decision was made to scrap all existing work and start the project from scratch. Add files are just some simple foundational code for the program.

---
+++ simpleEncrpytionMain.c
@@ -0,0 +1,24 @@
+#include "simpleEncrpytionHeader.h"
+
+int main() {
+
+    int choice;
+
+    //Simple Introduction Message
+    printf("\n\n--------\nSimple Encrpytion v0.1\n-------\n\n");
+    printf("--------\nPlease pick and option:\n1.Encrpt\n2.Decrypt\n-------\n");
+    scanf("%d", choice);
+
+    if (choice == 1){
+
+    }
+    else if (choice == 2){
+
+    }
+    else {
+        printf("--------\nERROR: Invalid Choice - Killing Program\n-------\n");
+    }
+
+
+    return 0;
+}
